b4b1042c50acc203aa2f1cf0c7ad2d11123b2605
Yan103 <yanfazleev@yandex.ru>
2025-03-18 00:48:11 +0300

    Added Makefile and .gitignore

--- main.c
+++ main.c
@@ -6,9 +6,7 @@ extern int myprintf(const char *fmt, ...) __attribute__ ((format(printf, 1, 2)))
 int main() {
     int a = myprintf("Test: %% %d %d %c\n", 224, -17, '!');
 
-    myprintf("%d %d\n", a, 0);
-
-    myprintf("%d\n", foo(15));
+    myprintf("%d\n", a);
 
     return 0;
 }
