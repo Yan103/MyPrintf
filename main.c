a8c0da5be6fed57edf706d1cf8f98498830519d8
Yan103 <yanfazleev@yandex.ru>
2025-03-18 01:47:51 +0300

    Added jump_table (work but a few segfault ^^)

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-// TODO MyPrintf + jmptbl + buffer + default case in jmptable + CDECL
-extern int myprintf(const char *fmt, ...) __attribute__ ((format(printf, 1, 2)));
+// TODO bufferization !
+extern int MyPrintf(const char *fmt, ...) __attribute__ ((format(printf, 1, 2)));
 
 int main() {
-    int a = myprintf("Test: %% %d %d %c\n", 224, -17, '!');
+    int a = MyPrintf("Test: %% %d %d %c\n", 224, -17, '!');
 
-    myprintf("%d\n", a);
+    MyPrintf("%d\n", a);
 
     return 0;
 }
--- myprintf.asm
+++ myprintf.asm
@@ -1,14 +1,36 @@
+section .rodata
+jump_table:
+                        dq case_percent         ; %
+times ('b' - '%' - 1)   dq case_default         ; skip useless [38-97 ASCII]
+                        dq case_b               ; %b
+                        dq case_c               ; %c
+                        dq case_d               ; %d
+
+times ('o' - 'd' - 1)   dq case_default         ; skip useless [101-111 ASCII]
+                        dq case_o               ; %o
+
+times ('s' - 'o' - 1)   dq case_default         ; skip useless [112-114 ASCII]
+
+                        dq case_s               ; %s
+
+times ('x' - 's' - 1)   dq case_default         ; skip useless [115-119 ASCII]
+
+                        dq case_x               ; %x
+
+                        dq case_default         ; default case for errors
+
+
 section .text
 
-global  myprintf
+global  MyPrintf
 
 ;=======================================================
 ;                 TRAMPLINE FOR CDeCL
 ;=======================================================
-myprintf:
+MyPrintf:
         pop r15
 
-        push r9             ; push args for cdcle format
+        push r9                                 ; push args for cdcle format
         push r8
         push rcx
         push rdx
@@ -24,81 +46,246 @@ myprintf:
 
 cdcle_printf:
         push rbp
-        mov rbp, rsp
+        mov  rbp, rsp
 
         push rbx
         push r10
 
         xor rcx, rcx
-        mov rbx, [rbp + 0x10]       ; format string
+        mov rbx, [rbp + 0x10]                   ; format string
 
 .str_loop:
-        mov al, [rbx]
-        test al, al
-        jz .end
+        cmp BYTE [rbx], '%'                     ; check if symb is specifier
+        je .specifier_encounter
 
-        cmp al, '%'
-        je .handle_specifier
+        push rcx                                ; protect from syscall
 
-        push rax
-        call putchar
+        mov rax, 0x01                           ; write64 (rdi, rsi, rax)
+        mov rdi, 0x01                           ; stdout fd
+        mov rsi, rbx                            ; curr string pos
+        mov rdx, 0x01                           ; display only 1 char
+        syscall
 
-        add rsp, 8
-        inc rbx
-        jmp .str_loop
+        pop rcx
 
-.handle_specifier:
-        inc rbx                     ; skip '%'
-        mov al, [rbx]
+.str_loop_end:
+        inc rbx                                 ; next symbol
 
-        cmp al, '%'
-        je .print_percent
+        cmp BYTE [rbx], 0                       ; stop if encounter null terminator
+        jne .str_loop
+        jmp .end
 
-        cmp al, 'd'
-        je .print_decimal
+.specifier_encounter:
+        inc rbx                         ; skip % symbol
 
-        cmp al, 'c'
-        je .print_char
+                                        ; value of specifier parameter in stack
+        push QWORD [rbp + rcx * 0x08 + 0x18]
+        push rbx                        ; specifier symbol
 
-        ;cmp al, 's'
-        ;je .print_string
+        call handle_specifier
 
-.print_percent:
-        push '%'
+        add  rsp, 2 * 0x08              ; clean stack
 
-        call putchar
+        cmp BYTE [rbx], '%'             ; <-- FORMER BUG, KEEP AN EYE ON THIS PLACE
+        jne .spend_stack_param
 
-        add rsp, 8
-        inc rbx
+        jmp .str_loop_end
 
-        jmp .str_loop
+.spend_stack_param:
+        inc rcx                         ; increase number of processed specifiers
+        jmp .str_loop_end
 
-.print_decimal:
-        push qword [rbp + 0x18 + rcx * 8]
-
-        call print_dec
-
-        add rsp, 8
-        inc rcx
-        inc rbx
-
-        jmp .str_loop
-
-.print_char:
-        push qword [rbp + 0x18 + rcx * 8]
-
-        call putchar
-
-        add rsp, 8
-        inc rcx
-        inc rbx
-
-        jmp .str_loop
+        jmp .end                   ; kinda silly precaution
 
 .end:
-        mov rax, rcx                ; processed argument count
 
-        pop r10
+        mov rax, rcx                    ; return value
+
+        pop r10                         ; restore
+        pop rbx
+        pop rcx
+
+        pop rbp
+
+        ret
+
+handle_specifier:
+        push rbp
+        mov  rbp, rsp
+
+        push rbx
+        push rcx
+
+        mov rbx, [rbp + 0x10]                   ; spec symbol
+
+        mov cl, BYTE [rbx]
+        xor rbx, rbx
+        mov bl, cl
+
+        cmp rbx, 'x'                            ; the greates ASCII code among all spec symbs
+        ja case_default
+
+        cmp rbx, '%'                            ; the least ASCII code
+        jb case_default
+
+        jmp [jump_table + 8 * (rbx - '%')]      ; jump with jump_table
+
+L1:  ; & ASCII 37
+L2:  ; '
+L3:  ; (
+L4:  ; )
+L5:  ; *
+L6:  ; +
+L7:  ; ,
+L8:  ; -
+L9:  ; .
+L10: ; /
+L11: ; 0
+L12: ; 1
+L13: ; 2
+L14: ; 3
+L15: ; 4
+L16: ; 5
+L17: ; 6
+L18: ; 7
+L19: ; 8
+L20: ; 9
+L21: ; :
+L22: ; ;
+L23: ; <
+L24: ; =
+L25: ; >
+L26: ; ?
+L27: ; @
+L28: ; A
+L29: ; B
+L30: ; C
+L31: ; D
+L32: ; E
+L33: ; F
+L34: ; G
+L35: ; H
+L36: ; I
+L37: ; J
+L38: ; K
+L39: ; L
+L40: ; M
+L41: ; N
+L42: ; O
+L43: ; P
+L44: ; Q
+L45: ; R
+L46: ; S
+L47: ; T
+L48: ; U
+L49: ; V
+L50: ; W
+L51: ; X
+L52: ; Y
+L53: ; Z
+L54: ; [
+L55: ; \
+L56: ; ]
+L57: ; ^
+L58: ; _
+L59: ; `
+L60: ; a
+L64: ; e
+L65: ; f
+L66: ; g
+L67: ; h
+L68: ; i
+L69: ; j
+L70: ; k
+L71: ; l
+L72: ; m
+L73: ; n
+L75: ; p
+L76: ; q
+L77: ; r
+L79: ; t
+L80: ; u
+L81: ; v
+L82: ; w ASCII 119
+
+case_default:
+        ; aboba
+
+        jmp switch_end
+
+case_percent:
+        push '%'
+        call putchar
+
+        add rsp, 8
+        jmp switch_end
+
+case_b:
+        push '0'                         ; write prefix
+        call putchar
+        add rsp, 0x08
+
+        push 'b'
+        call putchar
+        add rsp, 0x08
+
+        ; TODO print binary num
+
+        jmp switch_end
+
+case_c:
+        push QWORD [rbp + 0x18]
+        call putchar
+
+        add rsp, 0x08
+
+        jmp switch_end
+
+case_d:
+        push QWORD [rbp + 0x18]
+        call print_dec
+
+        add rsp, 0x08
+
+        jmp switch_end
+
+case_o:
+        push '0'                         ; write prefix
+        call putchar
+        add rsp, 0x08
+
+        push 'o'
+        call putchar
+        add rsp, 0x08
+
+        ; TODO print oct num
+
+        jmp switch_end
+
+case_s:
+        ;push QWORD [rbp + 0x18]
+        ;call puts_cdecl
+        ; TODO puts_cdecl
+
+        add rsp, 0x08
+
+        jmp switch_end
+
+case_x:
+        push '0'                         ; write prefix
+        call putchar
+        add rsp, 0x08
+
+        push 'x'
+        call putchar
+        add rsp, 0x08
+
+        ; TODO print hex num
+
+        jmp switch_end
+
+switch_end:
+        pop rcx
         pop rbx
         pop rbp
 
@@ -106,7 +293,7 @@ cdcle_printf:
 
 print_dec:
         push rbp
-        mov rbp, rsp
+        mov  rbp, rsp
 
         push rax
         push rcx
